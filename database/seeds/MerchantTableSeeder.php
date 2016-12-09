aeb3eba1b4020fcee33cc8769cc2d011de211b89
Nori Ohara <hideno0110@gmail.com>
2016-12-09 11:17:41 +0900

    fix layout

--- database/seeds/MerchantTableSeeder.php
+++ database/seeds/MerchantTableSeeder.php
@@ -74,7 +74,7 @@ class MerchantTableSeeder extends Seeder
           $merchant->save();       
 
           //Inventory
-          for ($j=0; $j < 10; $j++) {
+          for ($j=0; $j < 20; $j++) {
             echo $j;
           $input=  array(
               'merchant_id' => rand(1,4),
